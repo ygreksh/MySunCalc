60976cf6750782122d2d0e4843d2fa3bd37e6972
ygrek <ygreksh>
2018-01-12 22:59:24 +0200

    mainactivity

--- app/src/main/java/com/example/ygrek/mysuncalc/MainActivity.java
+++ app/src/main/java/com/example/ygrek/mysuncalc/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.ygrek.mysuncalc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
